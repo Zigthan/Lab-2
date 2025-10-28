614238b08a1d6350c37bdd6f8fce0d52552168cf
Ethan Daniel <EthanBaliwagDB42.24@ichat.sp.edu.sg>
2025-10-28 14:52:38 +0800

    Initial verion to display text message on console

--- bmi.py
+++ bmi.py
@@ -1,4 +1,18 @@
 def calculate_bmi(height, weight):
-    print("Height = " + height)
-    print("Weight = " + weight)
-calculate_bmi(weight=57, height=1.73)
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    BMI = weight / (height  * height)
+
+    print("BMI = " + str(BMI))
+
+    if BMI < 18.5:
+        print ("You are underweight.")
+    elif BMI >= 18.5 and BMI < 24.9:
+        print ("You are normal weight.")
+    elif BMI >=25 and BMI <29.9:
+        print ("You are overweight.")
+    else: 
+        print ("You are obese.")
+        
+calculate_bmi(weight=95, height=1.80)
